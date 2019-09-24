bb649f67e4d5659b58ad0e64aebca01c53e1e105
Maarten Franken <maartenfranken4@hotmail.com>
2019-09-24 21:04:43 +0200

    getShape method

--- src/domain/Reader.java
+++ src/domain/Reader.java
@@ -23,27 +23,12 @@ public class Reader {
 
     public ArrayList<Shape> readAll() {
         ArrayList<Shape> shapes = new ArrayList<>();
-        while(reader.hasNextLine()) {
-            List<String> parts = new ArrayList<>(Arrays.asList(reader.nextLine().split(":")));
+        ShapeController shapeController = new ShapeController();
 
-            switch(parts.get(0)) {
-                case "cone":
-                    shapes.add(new Cone(Double.parseDouble(parts.get(1)), Double.parseDouble(parts.get(2))));
-                    break;
-                case "cube":
-                    shapes.add(new Cube(Double.parseDouble(parts.get(1)), Double.parseDouble(parts.get(2)), Double.parseDouble(parts.get(3))));
-                    break;
-                case "cylinder":
-                    shapes.add(new Cylinder(Double.parseDouble(parts.get(1)), Double.parseDouble(parts.get(2))));
-                    break;
-                case "sphere":
-                    shapes.add(new Sphere(Double.parseDouble(parts.get(1))));
-                    break;
-                case "squarePyramid":
-                    shapes.add(new SquarePyramid(Double.parseDouble(parts.get(1)), Double.parseDouble(parts.get(2)), Double.parseDouble(parts.get(3))));
-                    break;
-            }
+        while(reader.hasNextLine()) {
+            shapes.add(shapeController.getShape(reader.nextLine()));
         }
+
         return shapes;
     }
 }
