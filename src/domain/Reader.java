82d49db08cc12e06cbd01f3a7ac4f7fb4e9f5af5
Jasper Helmich <jasper.helmich@gmail.com>
2019-10-08 16:26:30 +0200

    Added a conf file for the database connection

--- src/domain/Reader.java
+++ src/domain/Reader.java
@@ -19,6 +19,31 @@ public class Reader implements Closeable {
         }
     }
 
+    public Reader(File file) {
+        try {
+            this.reader = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public HashMap<String, String> readProperties() {
+        HashMap<String, String> properties = new HashMap<>();
+
+        while(reader.hasNextLine()) {
+            String prop = reader.nextLine();
+            List<String> parts = new ArrayList<>(Arrays.asList(prop.split("=", 2)));
+
+            if (parts.size() <= 1) {
+                properties.put(parts.get(0), "");
+            } else {
+                properties.put(parts.get(0), parts.get(1));
+            }
+        }
+
+        return properties;
+    }
+
     public ArrayList<Shape> readAll() {
         ArrayList<Shape> shapes = new ArrayList<>();
         ShapeController shapeController = new ShapeController();
